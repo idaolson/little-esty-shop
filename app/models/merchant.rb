1b34623ed8cf61287a4cf57843b51f86f55e6159
Ida Olson <idaolson@gmail.com>
2021-09-27 19:20:33 -0600

    Update merchant model with relationship to bulk_discount

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, through: :items
+  has_many :bulk_discounts
   validates :name, presence: true
 
   enum status: [:disabled, :enabled]
