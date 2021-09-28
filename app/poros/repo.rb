c500611425eba2c5a1232cd4a19a67c9e39d4260
Ida Olson <idaolson@gmail.com>
2021-09-28 15:04:44 -0600

    Update github api for new project

--- app/poros/repo.rb
+++ app/poros/repo.rb
@@ -2,9 +2,15 @@ class Repo
   attr_reader :name, :pulls, :contributors
 
   def initialize(repo_data)
-    @name = repo_data[:name][:name].split("-").join(" ").capitalize
+    @name = format_name(repo_data[:name][:name])
     @pulls = repo_data[:pulls].count
     @contributors = repo_data[:contributors]
     @commits = repo_data[:commits]
+  end
+
+  def format_name(data)
+    split_data = data.split("-")
+    capitalized = split_data.map { |string| string.capitalize}
+    capitalized.join(" ")
   end
 end
